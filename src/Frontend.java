62063cfd5c4588e1a656766dc6c723d544d84a0c
RyanEweSeng <ryantoh1001@gmail.com>
2021-04-10 03:17:33 -0500

    implemented menu ui

--- src/Frontend.java
+++ src/Frontend.java
@@ -16,6 +16,11 @@ import java.util.Scanner;
 public class Frontend {
     private static Backend b;
 
+    private static String appHeader =
+        "=============================================\n" +
+        "           Cheapest Train Path App           \n" +
+        "=============================================";
+
     /**
      * Main app loop.
      * @param args
@@ -67,20 +72,49 @@ public class Frontend {
      * This method will have the UI for the main menu.
      */
     public static void printMainMenu() {
-        // TODO
+        System.out.println(appHeader);
+        System.out.println("Welcome to the cheapest train path app!");
+        System.out.println();
+        System.out.println("Enter ‘s’ to input start and end destinations");
+        System.out.println("Enter ‘q’ to quit the app");
+        System.out.println("=============================================");
     }
 
     /**
      * This method will have the UI for the input menu.
      */
     public static void printInputMenu() {
-        // TODO
+        System.out.println(appHeader);
+        System.out.println("Enter start and end destinations using the format: <start-city> <end-city>");
+        System.out.println();
+        System.out.println("Enter ‘x’ to return to main menu");
+        System.out.println("=============================================");
     }
 
     /**
      * This method will have the UI for the data menu.
      */
     public static void printDataMenu() {
-        // TODO
+        final int WIDTH = 27;
+        String totalCost = String.valueOf(b.getTotalCosts());
+        totalCost = "$" + totalCost;
+
+        // add proper spacing for total cost
+        int spacing = (27 - totalCost.length()) / 2;
+        if ((27 - totalCost.length()) % 2 == 1) {// odd spacing we add the extra spacing to the right
+            totalCost += " ";
+        }
+        for (int i = 0; i < spacing; i++) {
+            totalCost = " " + totalCost + " ";
+        }
+
+        System.out.println(appHeader);
+        System.out.println();
+        System.out.println("----------------------------------------------------------------------------------");
+        System.out.println("|                   Total Cost                       |"    +   totalCost   +    "|");
+        System.out.println("----------------------------------------------------------------------------------");
+        System.out.println("|          City          |      Individual Cost      |      Cumulative Cost      |");
+        System.out.println("----------------------------------------------------------------------------------");
+        // TODO data for the rest of the table
     }
 }
