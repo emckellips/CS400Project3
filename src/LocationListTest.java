e0280deea46aec47bb7ecf9fc14eb40e8cfba50e
alonergan <79943132+alonergan@users.noreply.github.com>
2021-04-13 17:23:41 -0500

    Added file header

--- src/LocationListTest.java
+++ src/LocationListTest.java
@@ -1,3 +1,11 @@
+// --== CS400 File Header Information ==--
+// Name: Aidan Lonergan
+// Email: alonergan@wisc.edu
+// Team: IG Red
+// Role: DataWrangler
+// TA: Sid
+// Lecturer: Gary Dahl
+// Notes to Grader: n/a
 import static org.junit.jupiter.api.Assertions.*;
 import java.io.File;
 import java.io.FileNotFoundException;
